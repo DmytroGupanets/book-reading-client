e4a0aaabddc170731c866c3c8a9013fc82161984
Afrikanochka <Mtgnatali@gmail.com>
2021-10-14 13:37:39 +0300

    return commit

--- src/components/container/ContainerStyled.js
+++ src/components/container/ContainerStyled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const ContainerStyled = styled.div`
   padding-left: 25px;
   padding-right: 25px;
-  /* margin: 0 auto; */
+  margin: 0 auto;
   max-width: 320px;
 
   @media screen and (min-width: 768px) {

cd4f260750743d003a21af87001ec1e6ddd60409
Alina Akompi <alina.akompi@gmail.com>
2021-10-16 02:26:30 +0300

    add reg text markup

--- src/components/auth/Registration/Registration.jsx
+++ src/components/auth/Registration/Registration.jsx
@@ -1,5 +1,6 @@
 import arrowIcon from "../../../images/auth/arrow.svg";
 import RegistrationForm from "../RegistrationForm/RegistrationForm";
+import { RegistrationStyled } from "./RegistrationStyled";
 
 const Registration = () => {
   return (
@@ -8,51 +9,51 @@ const Registration = () => {
         <RegistrationForm />
       </div>
       <div className="rightSideContainer">
-        <div>
-          <h2>Books Reading</h2>
-          <p>Допоможе вам</p>
-          <ul>
-            <li>
+        <RegistrationStyled>
+          <h2 className="title">Books Reading</h2>
+          <p className="listTitle">Допоможе вам</p>
+          <ul className="list">
+            <li className="listItem">
               <svg className="arrowIcon" width="5" height="10">
                 <use href={arrowIcon + "#icon-arrow"}></use>
               </svg>
               Швидше сформулювати свою ціль і розпочати читати
             </li>
-            <li>
+            <li className="listItem">
               <svg className="arrowIcon" width="5" height="10">
                 <use href={arrowIcon + "#icon-arrow"}></use>
               </svg>
               Пропорційно розподілити навантаження на кожний день
             </li>
-            <li>
+            <li className="listItem">
               <svg className="arrowIcon" width="5" height="10">
                 <use href={arrowIcon + "#icon-arrow"}></use>
               </svg>
               Відстежувати особистий успіх
             </li>
           </ul>
-          <p>Також ви зможете</p>
-          <ul>
-            <li>
+          <p className="listTitle">Також ви зможете</p>
+          <ul className="list">
+            <li className="listItem">
               <svg className="arrowIcon" width="5" height="10">
                 <use href={arrowIcon + "#icon-arrow"}></use>
               </svg>
               Формувати особисту думку незалежну від інших
             </li>
-            <li>
+            <li className="listItem">
               <svg className="arrowIcon" width="5" height="10">
                 <use href={arrowIcon + "#icon-arrow"}></use>
               </svg>
               Підвищити свої професійні якості опираючись на нові знання
             </li>
-            <li>
+            <li className="listItem">
               <svg className="arrowIcon" width="5" height="10">
                 <use href={arrowIcon + "#icon-arrow"}></use>
               </svg>
               Стати цікавим співрозмовником
             </li>
           </ul>
-        </div>
+        </RegistrationStyled>
       </div>
     </div>
   );

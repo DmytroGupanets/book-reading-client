9915500ee58ccbca18fcedd086f5ea6e84f59dcb
Gupanets Dmytro <dmgpnts@gmail.com>
2021-10-16 09:47:32 +0300

    add container to pages

--- src/pages/libraryPage/LibraryPage.js
+++ src/pages/libraryPage/LibraryPage.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Library from "../../components/library/Library";
-// import Library from "../../components/library/Library";
+import Container from "../../components/container/Container";
+import LibraryEmpty from "../../components/libraryEmpty/LibraryEmpty";
 
 const LibraryPage = () => {
   return (
-    <>
+    <Container>
+      <LibraryEmpty />
       <Library />
-      {/* <AddBooks/> */}
-      {/* <BooksList/> */}
-    </>
+    </Container>
   );
 };
 

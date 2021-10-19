5e5ee0407c02dd327544dee136ee8ff676f127bf
Alina Akompi <alina.akompi@gmail.com>
2021-10-20 02:59:51 +0300

    Mobile auth

--- src/components/auth/Registration/RegistrationStyled.js
+++ src/components/auth/Registration/RegistrationStyled.js
@@ -1,60 +1,49 @@
 import styled from "styled-components";
 
 export const RegistrationStyled = styled.div`
-  padding: 30px 25px 58px 25px;
-  font-style: normal;
-  letter-spacing: 0;
-  color: #242a37;
-
-  .title {
-    margin-bottom: 20px;
-    text-align: center;
-    font-family: Abril Fatface, sans-serif;
-    font-weight: normal;
-    font-size: 34px;
-    line-height: 1.12;
-  }
-
-  .listTitle {
-    margin-bottom: 14px;
-    font-family: Montserrat, sans-serif;
-    font-weight: 500;
-    font-size: 20px;
-    line-height: 1.9;
-  }
-
-  .list {
-    margin-bottom: 20px;
-  }
-
-  .listItem {
-    font-family: Montserrat, sans-serif;
-    font-style: normal;
-    font-weight: 500;
-    font-size: 14px;
-    line-height: 0.9;
-    color: #898f9f;
-  }
-
-  .arrowIcon {
-    width: 5px;
-    height: 10px;
-    margin-right: 12px;
-  }
+  display: none;
 
   @media screen and (min-width: 768px) {
+    display: block;
     padding: 60px 60px 60px 100px;
+    font-style: normal;
+    letter-spacing: 0;
+    color: #242a37;
 
     .title {
       margin-bottom: 40px;
+      text-align: center;
+      font-family: Abril Fatface, sans-serif;
+      font-weight: normal;
+      font-size: 34px;
+      line-height: 1.12;
     }
 
     .listTitle {
       margin-bottom: 4px;
+      font-family: Montserrat, sans-serif;
+      font-weight: 500;
+      font-size: 20px;
+      line-height: 1.9;
     }
 
     .listItem {
+      font-family: Montserrat, sans-serif;
+      font-style: normal;
+      font-weight: 500;
+      font-size: 14px;
       line-height: 2.71;
+      color: #898f9f;
+    }
+
+    .list {
+      margin-bottom: 20px;
+    }
+
+    .arrowIcon {
+      width: 5px;
+      height: 10px;
+      margin-right: 12px;
     }
   }
 

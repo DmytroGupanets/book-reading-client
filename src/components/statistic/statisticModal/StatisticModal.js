0bc28ac64ba69671b115a8ab2609c7d8095bde36
Yulia Dobrova <dobrova2009@gmail.com>
2021-10-19 23:24:31 +0300

    modal quotes and logo animation added

--- src/components/statistic/statisticModal/StatisticModal.js
+++ src/components/statistic/statisticModal/StatisticModal.js
@@ -2,6 +2,19 @@ import React from "react";
 import StatisticModalStyled from "./StatisticModalStyled";
 import sprite from "./sprite.svg";
 import Modal from "../../modal/Modal";
+import quotes from "../../quotes/ua/quotesUA.json";
+import quotesIcon from "../../../images/auth/quotes.svg";
+
+const quotesArray = JSON.parse(JSON.stringify(quotes));
+const min = 1; // включая
+const max = 41; // исключая
+const randomInt = Math.floor(Math.random() * (max - min) + min);
+const quotesArrayElement = quotesArray[randomInt];
+// console.log(`quotesArrayElement`, quotesArrayElement);
+const randomQuote = Object.values(quotesArrayElement)[1];
+const randomAuthor = Object.values(quotesArrayElement)[2];
+// console.log(randomQuote);
+// console.log(randomAuthor);
 
 const StatisticModal = ({ onClose }) => {
   return (
@@ -15,6 +28,13 @@ const StatisticModal = ({ onClose }) => {
             Ти молодчина, <br /> але потрібно швидше! <br /> Наступного разу
             тобі все вдасться)
           </p>
+          <div className="StatisticModalQuote">
+            <svg className="quotesIcon" width="25" height="20">
+              <use href={quotesIcon + "#icon-quotes"}></use>
+            </svg>
+            <p className="StatisticModalQuoteText">"{randomQuote}"</p>
+            <p className="StatisticModalAuthorText">{randomAuthor}</p>
+          </div>
           <button
             type="button"
             className="StatisticModalButton"
--- src/components/statistic/statisticModal/StatisticModalStyled.js
+++ src/components/statistic/statisticModal/StatisticModalStyled.js
@@ -2,12 +2,15 @@ import styled from "styled-components";
 
 const StatisticModalStyled = styled.div`
   .StatisticModalWrapper {
-    width: 390px;
-    height: 290px;
-    /* border: 1px solid; */
+    width: 270px;
+    height: 500px;
     border-color: none;
     background-color: #ffffff;
     box-shadow: 10px 10px 20px rgba(9, 30, 63, 0.2);
+    @media screen and (min-width: 768px) {
+      width: 390px;
+      height: 500px;
+    }
   }
   .StatisticModalIcon {
     width: 50px;
@@ -16,7 +19,6 @@ const StatisticModalStyled = styled.div`
     margin-bottom: 21px;
     overflow: visible;
     fill: #a6abb9;
-    /* transition: transform 250ms linear; */
 
     animation-name: StatisticModalIconColor;
     animation-duration: 2000ms;
@@ -38,8 +40,8 @@ const StatisticModalStyled = styled.div`
   }
 
   .StatisticModalText {
-    width: 296px;
-    height: 66px;
+    width: 240px;
+    height: 80px;
     font-family: Montserrat;
     font-style: normal;
     font-weight: 500;
@@ -49,9 +51,68 @@ const StatisticModalStyled = styled.div`
     margin-left: auto;
     margin-right: auto;
     margin-bottom: 25px;
+    @media screen and (min-width: 768px) {
+      width: 296px;
+      height: 66px;
+    }
   }
+  .StatisticModalQuote {
+    width: 230px;
+    height: 160px;
+    background-image: url("https://i.ibb.co/jhR2LP4/imgonline-com-ua-Pic-On-Pic-Wnl-Es2-K5ypg-Oe.png");
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 25px;
+    padding-left: 5px;
+    padding-right: 5px;
+    box-shadow: 10px 10px 20px rgba(9, 30, 63, 0.4);
+    @media screen and (min-width: 768px) {
+      width: 300px;
+      height: 180px;
+    }
+  }
+  .quotesIcon {
+    margin-top: 10px;
+    /* margin-bottom: 10px; */
+  }
+  .StatisticModalQuoteText {
+    padding: 7px;
+    margin-bottom: 5px;
+    font-size: 14px;
+    line-height: 1.2;
+    color: #f6f7fb;
+    @media screen and (min-width: 768px) {
+      font-size: 15px;
+    }
+  }
+
+  .StatisticModalAuthorText {
+    position: relative;
+    padding-top: 5px;
+    color: #f6f7fb;
+    font-size: 13px;
+    line-height: 2.7;
+    margin-bottom: 15px;
+    @media screen and (min-width: 768px) {
+      font-size: 14px;
+    }
+
+    &::before {
+      position: absolute;
+      display: block;
+      content: "";
+      top: 0;
+      left: 50%;
+      right: 50%;
+      width: 150px;
+      /* border: 1px solid rgba(36, 42, 55, 0.25); */
+      border: 1px solid rgba(246, 247, 251, 0.25);
+      transform: translate(-50%, -50%);
+    }
+  }
+
   .StatisticModalButton {
-    width: 130px;
+    width: 100px;
     height: 40px;
     background-color: #ff6b08;
     font-family: Montserrat;
@@ -65,6 +126,9 @@ const StatisticModalStyled = styled.div`
     box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
     cursor: pointer;
     transition: transform 250ms linear;
+    @media screen and (min-width: 768px) {
+      width: 130px;
+    }
   }
 
   .StatisticModalButton:hover,

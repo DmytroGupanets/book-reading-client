1f9122a44a08247bef6e981cba0026fa4f063a79
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-10-21 20:49:59 +0300

    render remain of readed books

--- src/components/training/targetRead/TargetRead.js
+++ src/components/training/targetRead/TargetRead.js
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { getInProgressdBooks } from "../../../redux/books/booksSelectors";
-import { endDate, startDate } from "../../../redux/target/targetSelectors";
+import {
+  endDate,
+  getIdxOfReadedBooksInTraining,
+  startDate,
+} from "../../../redux/target/targetSelectors";
 import useDate from "../../../hooks/useDate";
 
 import TargetReadStyled from "./TargetReadStyled";
 import colors from "../../../styles/colors";
 
 const TargetRead = ({ state }) => {
-  const readingBooks = 3;
-
   const [
     stateData,
     moment,
@@ -21,6 +23,7 @@ const TargetRead = ({ state }) => {
   const start = useSelector(startDate);
   const end = useSelector(endDate);
   const booksInProgress = useSelector(getInProgressdBooks);
+  const idxOfReadedBooksInTraining = useSelector(getIdxOfReadedBooksInTraining);
 
   useEffect(() => {}, []);
 
@@ -45,7 +48,14 @@ const TargetRead = ({ state }) => {
         </li>
         {state && (
           <li className="targetReadItem">
-            <div className="targetReadDigRemain"> {readingBooks}</div>
+            <div className="targetReadDigRemain">
+              {idxOfReadedBooksInTraining === -1
+                ? booksInProgress.length
+                : booksInProgress.slice(
+                    idxOfReadedBooksInTraining,
+                    booksInProgress.length - 1
+                  ).length}
+            </div>
             <p className="targetReadItemDescription">Залишилось книжок</p>
           </li>
         )}

8080cc12101cbe88af3510bf9948be672e3a2ce1
Gupanets Dmytro <dmgpnts@gmail.com>
2021-10-22 22:17:25 +0300

    fix styles error

--- src/components/libraryEmpty/LibraryEmpty.js
+++ src/components/libraryEmpty/LibraryEmpty.js
@@ -1,48 +1,46 @@
-
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useFormik } from "formik";
-import { useDispatch } from 'react-redux';
-import { addNewBookOperation } from '../../redux/books/booksOperations';
-import { LibraryEmptyStyled } from './LibraryEmptyStyled';
-import { libraryValidationSchema } from './validationLibrary/validationSchema';
+import { useDispatch } from "react-redux";
+import { addNewBookOperation } from "../../redux/books/booksOperations";
+import { LibraryEmptyStyled } from "./LibraryEmptyStyled";
+import { libraryValidationSchema } from "./validationLibrary/validationSchema";
 
 import { useContext } from "react";
 import { ThemeContext } from "../App";
 import { useTranslation } from "react-i18next";
 
 const LibraryEmpty = () => {
-    const dispatch = useDispatch();
-    const { t } = useTranslation();
-    const { theme } = useContext(ThemeContext);
-   
-    const formik = useFormik({
-        initialValues: {
-            title: "",
-            author: "",
-            year: "",
-            pages: "",    
-        },
-        validationSchema: libraryValidationSchema,
+  const dispatch = useDispatch();
+  const { t } = useTranslation();
+  const { theme } = useContext(ThemeContext);
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      author: "",
+      year: "",
+      pages: "",
+    },
+    validationSchema: libraryValidationSchema,
 
     onSubmit: ({ title, author, year, pages }) => {
-        const newBook = {
-           name: title,
-           author,
-           year,
-           pages,
-       };
+      const newBook = {
+        name: title,
+        author,
+        year,
+        pages,
+      };
 
-       dispatch(addNewBookOperation(newBook));
-      },
-    });
-    
-    return (
-        
-        <LibraryEmptyStyled  onSubmit={formik.handleSubmit}>
-            <div className="bookContainer">
-             <label className="aboutBook aboutBook-title" htmlFor="title">
-             Назва книги
-             <input 
+      dispatch(addNewBookOperation(newBook));
+    },
+  });
+
+  return (
+    <LibraryEmptyStyled onSubmit={formik.handleSubmit} colors={theme}>
+      <div className="bookContainer">
+        <label className="aboutBook aboutBook-title" htmlFor="title">
+          Назва книги
+          <input
             id="title"
             type="text"
             name="title"
@@ -51,70 +49,71 @@ const LibraryEmpty = () => {
             className="aboutBookInput aboutBookInput-title"
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
-            />
-            </label>
-            <div className="aboutBookContainer">
-            <label className="aboutBook aboutBook-author" htmlFor="author">
+          />
+        </label>
+        <div className="aboutBookContainer">
+          <label className="aboutBook aboutBook-author" htmlFor="author">
             Автор книги
-            <input 
-            id="author"
-            type="text"
-            minLength="1"
-            maxLength="50"
-            name="author"
-            value={formik.values.author}
-            placeholder="..."
-            className="aboutBookInput aboutBookInput-author"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
+            <input
+              id="author"
+              type="text"
+              minLength="1"
+              maxLength="50"
+              name="author"
+              value={formik.values.author}
+              placeholder="..."
+              className="aboutBookInput aboutBookInput-author"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
             {formik.errors.title && formik.touched.title && (
-            <div className="inputError">{formik.errors.title}</div>
-          )}
-            </label>
-            <label className="aboutBook aboutBook-year" htmlFor="year">
+              <div className="inputError">{formik.errors.title}</div>
+            )}
+          </label>
+          <label className="aboutBook aboutBook-year" htmlFor="year">
             Рік випуску
-            <input 
-            id="year"
-            type="number"
-            name="year"
-            value={formik.values.year}
-            placeholder="..."
-            className="aboutBookInput aboutBookInput-year"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
+            <input
+              id="year"
+              type="number"
+              name="year"
+              value={formik.values.year}
+              placeholder="..."
+              className="aboutBookInput aboutBookInput-year"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
-            </label>
-            <label className="aboutBook aboutBook-page" htmlFor="pages">
+          </label>
+          <label className="aboutBook aboutBook-page" htmlFor="pages">
             Кількість сторінок
             <input
-            id="pages"
-            type="number"
-            name="pages"
-            value={formik.values.pages}
-            placeholder="..."
-            className="aboutBookInput aboutBookInput-page"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
+              id="pages"
+              type="number"
+              name="pages"
+              value={formik.values.pages}
+              placeholder="..."
+              className="aboutBookInput aboutBookInput-page"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
-            </label>
-            </div>
-            <button type="submit" className="addButton">Додати</button>
-            </div>
+          </label>
+        </div>
+        <button type="submit" className="addButton">
+          Додати
+        </button>
+      </div>
+    </LibraryEmptyStyled>
+  );
+};
 
-        </LibraryEmptyStyled>
-        );
-    }
-    
-    export default LibraryEmpty;
-    
-    // const initialState = {
-    //        title: "",
-    //        author: "",
-    //        year: "",
-    //        pages: ""    
-    // }
-    // const LibraryEmpty = () => {
+export default LibraryEmpty;
+
+// const initialState = {
+//        title: "",
+//        author: "",
+//        year: "",
+//        pages: ""
+// }
+// const LibraryEmpty = () => {
 //     const [state, setState] = useState(initialState);
 //     const dispatch = useDispatch()
 
@@ -136,12 +135,12 @@ const LibraryEmpty = () => {
 //     }
 
 //     return (
-        
+
 //         <LibraryEmptyStyled  onSubmit={onHandleSubmit}>
 //             <div className="bookContainer">
 //             <label className="aboutBook aboutBook-title">
 //             Назва книги
-//             <input 
+//             <input
 //             required
 //             id="title"
 //             type="text"
@@ -157,7 +156,7 @@ const LibraryEmpty = () => {
 //             <div className="aboutBookContainer">
 //             <label className="aboutBook aboutBook-author">
 //             Автор книги
-//             <input 
+//             <input
 //             required
 //             id="author"
 //             type="text"
@@ -172,7 +171,7 @@ const LibraryEmpty = () => {
 //             </label>
 //             <label className="aboutBook aboutBook-year">
 //             Рік випуску
-//             <input 
+//             <input
 //             required
 //             id="year"
 //             type="number"
@@ -186,7 +185,7 @@ const LibraryEmpty = () => {
 //             <label className="aboutBook aboutBook-page">
 //             Кількість сторінок
 //             <input
-//             required 
+//             required
 //             id="pages"
 //             type="number"
 //             name="pages"
@@ -201,10 +200,8 @@ const LibraryEmpty = () => {
 //             </div>
 
 //         </LibraryEmptyStyled>
-        
+
 //     );
 // }
 
 // export default LibraryEmpty;
-    
-

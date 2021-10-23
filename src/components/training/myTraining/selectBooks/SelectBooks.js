8c2ae17007050d1e27509d74b23090fac29e1899
Vladyslav Bochkovskyi <bochkovskyi.vladyslav@gmail.com>
2021-10-23 12:33:05 +0300

    31231

--- src/components/training/myTraining/selectBooks/SelectBooks.js
+++ src/components/training/myTraining/selectBooks/SelectBooks.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Select from "react-select";
 import { getPlannedBooks } from "../../../../redux/books/booksSelectors";
 import { useDispatch, useSelector } from "react-redux";
@@ -15,51 +15,55 @@ import {
 import { useStickyState } from "../../../../hooks";
 
 const SelectBooks = () => {
+  // const { t } = useTranslation();
   const dispatch = useDispatch();
   const books = useSelector(getPlannedBooks);
   const plannedBooks = useSelector(getAllPlannedBooks);
   const selectedBooks = useSelector(getAllSelectedBooks);
 
-  const [value, setValue] = useState(books);
-  const [selectedBook, setSelectedBook] = useStickyState(
-    [selectedBooks],
-    "kekw"
-  );
+  const [value, setValue] = useStickyState(books, "book");
+  const [selectedBook, setSelectedBook] = useState({});
 
-  // useEffect(() => {
-  //   dispatch(setPlannedBooksForSelect(books));
-  // }, [books, dispatch]);
+  useEffect(() => {
+    dispatch(setPlannedBooksForSelect(books));
+  }, []);
 
   useEffect(() => {
     setValue(plannedBooks);
-  }, [plannedBooks]);
+  }, [plannedBooks, setValue]);
 
-  const handleInputChange = (value, e) => {
+  const handleInputChange = useCallback((value, e) => {
     if (e.action === "input-change") {
-      setSelectedBook((state) => [...state, { value }]);
+      setSelectedBook((state) => [...state, value]);
     }
-  };
+  }, []);
 
   const onChange = (e) => {
     const value = e.value;
-    setSelectedBook([value]);
+    setSelectedBook(value);
   };
 
-  const onHandleClick = (e) => {
-    dispatch(addSelectedBook(selectedBook));
-  };
+  const onHandleClick = useCallback(
+    (e) => {
+      dispatch(addSelectedBook(selectedBook));
+    },
+    [dispatch, selectedBook]
+  );
 
   const options = value.map(({ name, author, year, pages, _id }) => ({
     value: { name, author, year, pages, _id },
     label: name,
   }));
 
-  const onHandleDelete = (e) => {
-    const bookId = e.currentTarget.getAttribute("bookid");
-    const bookToRemove = books.find((book) => book._id === bookId);
+  const onHandleDelete = useCallback(
+    (e) => {
+      const bookId = e.currentTarget.getAttribute("bookid");
+      const bookToRemove = books.find((book) => book._id === bookId);
 
-    dispatch(removeSelectedBook(bookToRemove));
-  };
+      dispatch(removeSelectedBook(bookToRemove));
+    },
+    [books, dispatch]
+  );
 
   return (
     <>
@@ -69,7 +73,7 @@ const SelectBooks = () => {
         onChange={onChange}
         onInputChange={handleInputChange}
       />
-      <MyGoalList data={selectedBook} onClickDelete={onHandleDelete} />
+      <MyGoalList data={selectedBooks} onClickDelete={onHandleDelete} />
       <button onClick={onHandleClick}>Додати</button>
     </>
   );
